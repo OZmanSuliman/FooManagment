c92f40930a809debcfc24b811eaeb475d272cf86
OZmanSuliman <oz.solomon99@gmail.com>
2023-04-29 10:17:18 +0400

    add services

---
+++ FooManagement/Core/Services/CoreServices/ServicesFactory.swift
@@ -0,0 +1,33 @@
+//
+//  ServicesFactory.swift
+//  FooManagement
+//
+//  Created by Osman Ahmed on 29/04/2023.
+//
+
+import Foundation
+
+class ServicesFactory {
+    static let shared = ServicesFactory()
+
+    func makeTestNetworkService() -> TestNetworkServiceProtocol {
+        return TestNetworkService(networkingManager: makeNetworkService(), keychainService: makeKeychainService())
+    }
+
+    func makeKeychainService() -> KeychainProtocol {
+        return KeychainService()
+    }
+
+    func makeDatabaseService() -> DatabaseProtocol {
+        return DatabaseService()
+    }
+    
+    private func makeErrorParser() -> APIErrorParsing {
+        return APIErrorParser()
+    }
+
+    private func makeNetworkService() -> NetworkingProtocol {
+        return NetworkService(errorParser: makeErrorParser(), keychainService: makeKeychainService())
+    }
+}
+
